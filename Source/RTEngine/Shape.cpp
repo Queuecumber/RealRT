10506a360134493cee9c2a3ab0938685dbc97355
Max Ehrlich <max.ehr@gmail.com>
2013-12-07 01:03:31 -0500

    Removed phong BRDF as it is implemented in its own file now

--- Source/RTEngine/Shape.cpp
+++ Source/RTEngine/Shape.cpp
@@ -1,80 +1,25 @@
-#include "shape3D.h"
+#include "Shape.hpp"
 
 
+using namespace RealRT;
 
-
-double Phong(gmath::vector<3,double> &in, gmath::vector<3,double> &out, gmath::vector<3,double> &unorm, MATERIAL<> &mat)
+Shape::Shape(const Material &mat)
+    : _Material(mat)
 {
-	double ratio = 0.0;
 
-	//take the dot product of the unit normal against the vector to the light. Note that
-	//since both are unit vectors, this value is the cosine of the angle between them
-	double dot = unorm * out;
-
-	//use only positive cosine values to shade only the parts facing the light source
-	//
-	if(dot > 0)
-	{
-		if(mat.diffuseScaler > 0)
-		{
-			ratio += dot * mat.diffuseScaler;
-		}
-	}
-
-	vector<3,double> reflectionDir;
-	reflectionDir = out - 2.0 * (out * unorm) * unorm;
-	reflectionDir = reflectionDir.normalize();
-	dot = reflectionDir * in;
-
-	if(dot > 0)
-	{
-		if(mat.specularScaler > 0)
-		{
-			ratio += pow(dot,50) * mat.specularScaler;
-		}
-	} 
-
-	return ratio;
 }
 
-shape3D::shape3D(MATERIAL<> mat)
+Shape::~Shape(void)
 {
-	this->mat = mat;
-	locked = false;
+
 }
 
-shape3D::shape3D(PMATERIAL mat)
+inline bool Shape::IsLight(void) const
 {
-	this->mat = *mat;
-
-	locked = false;
+    return false;
 }
 
-shape3D::~shape3D(void){}
-
-PMATERIAL shape3D::LockMaterial()
+const Material &Shape::Material(void) const
 {
-	if(!locked)
-	{
-		locked=true; 
-		return &mat;
-	} 
-	
-	return &mat;
+    return _Material;
 }
-
-bool shape3D::UnlockMaterial(PMATERIAL mat)
-{
-	if(!locked)
-		return true;
-	
-	if(mat == &(this->mat))
-	{
-		mat = NULL;
-		locked = false; 
-		return true;
-	}else{
-		return false;
-	} 
-}
-
--- Source/RTEngine/Shape.hpp
+++ Source/RTEngine/Shape.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Material.hpp"
 #include "Ray.hpp"
 #include <opencv2/opencv.hpp>
 
@@ -24,21 +25,18 @@ namespace RealRT
 class Shape
 {
 public:
-    Shape(MATERIAL<> mat);
+    Shape(const Material &mat);
     virtual ~Shape(void);
 																			
     virtual double Intersect(const Ray &incident, bool &flipNormals) const = 0; //override to return the distance from the origion to the point of intersection
     virtual cv::Vec3d Normal(const cv::Point3d &pt) const = 0;	//override to get the UNIT normal at the given point
 
-    virtual bool IsLight(void) const { return false; }  //return true if this shape is a light source
+    virtual bool IsLight(void) const;  //return true if this shape is a light source
 
-	//locks the material and returns a pointer to it, or returns NULL if the material is locked
-	PMATERIAL LockMaterial();
-	bool UnlockMaterial(PMATERIAL mat);
+    const Material &Material(void) const;
 
 private:
-	MATERIAL<> mat;
-	bool locked;
+        const Material &_Material;
 };
 
 }
